1656d7661f0606494a2cb480b07d911e154eccba
Satyam Vishwakarma <v.satyam@op.iitg.ac.in>
2025-09-05 23:18:41 +0530

    change type 4

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,3 @@
-// src/app/blog/[slug]/page.tsx
 import { getBlogPostBySlug } from "../../../../lib/contentful";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import { notFound } from "next/navigation";
@@ -7,20 +6,17 @@ interface Params {
   slug: string;
 }
 
-export default async function BlogPostPage({ params }: { params: Params }) {
+export default async function BlogPostPage(props: { params: Params }) {
+  const { params } = props;
   const blog = await getBlogPostBySlug(params.slug);
 
   if (!blog) return notFound();
 
   return (
     <article className="max-w-3xl mx-auto py-12">
-      <h1 className="font-serif text-3xl font-light tracking-wide mb-3">
-        {blog.title}
-      </h1>
+      <h1 className="font-serif text-3xl font-light tracking-wide mb-3">{blog.title}</h1>
       <p className="text-gray-500 mb-6">{blog.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: documentToHtmlString(blog.content) }}
-      />
+      <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(blog.content) }} />
     </article>
   );
 }
