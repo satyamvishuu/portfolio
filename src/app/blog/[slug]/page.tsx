c4292d3d4a16e90f43c1df5fce1f9d89bbe70d9a
Satyam Vishwakarma <v.satyam@op.iitg.ac.in>
2025-09-06 00:31:14 +0530

    remove null condition 2

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import { getBlogPostBySlug } from "../../../../lib/contentful";
 import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 import { notFound } from "next/navigation";
+import { BLOCKS, type Document } from "@contentful/rich-text-types";
 
 export default async function BlogPostPage(props: any) {
-  // Await params because it's now a Promise
   const params = await props.params;
   console.log("Received slug:", params.slug);
 
@@ -12,11 +12,21 @@ export default async function BlogPostPage(props: any) {
 
   if (!blog) return notFound();
 
+  const fallbackDocument: Document = {
+    nodeType: BLOCKS.DOCUMENT,
+    data: {},
+    content: [],
+  };
+
   return (
-    <article className="max-w-3xl mx-auto py-12">
+    <article className="max-w-3xl mx-auto py-12 px-4">
       <h1 className="font-serif text-3xl font-light tracking-wide mb-3">{blog.title}</h1>
       <p className="text-gray-500 mb-6">{blog.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(blog.content) }} />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: documentToHtmlString(blog.content ?? fallbackDocument),
+        }}
+      />
     </article>
   );
 }
