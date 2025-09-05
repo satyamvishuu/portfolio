d1cc912c69162883a339e9db4a4437999d458d21
Satyam Vishwakarma <v.satyam@op.iitg.ac.in>
2025-09-05 23:15:20 +0530

    change type 2

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,24 +1,27 @@
 import { getBlogPostBySlug } from "../../../../lib/contentful";
-// import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
-// import { notFound } from "next/navigation";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+import { notFound } from "next/navigation";
 
-interface BlogPostPageProps {
+export default async function BlogPostPage({
+  params,
+}: {
   params: { slug: string };
-}
-
-export default async function BlogPostPage({ params }: BlogPostPageProps) {
+}) {
   const blog = await getBlogPostBySlug(params.slug);
 
-  if (!blog) {
-    return <div>Blog not found</div>;
-  }
+  if (!blog) return notFound();
 
   return (
-    <main className="max-w-3xl mx-auto py-12">
-      <h1 className="font-serif text-3xl font-light mb-4">{blog.title}</h1>
-      <p className="text-gray-500 mb-2">{blog.date}</p>
-      <div>{blog.excerpt}</div>
-      {/* Render blog.content with @contentful/rich-text-react-renderer */}
-    </main>
+    <article className="max-w-3xl mx-auto py-12">
+      <h1 className="font-serif text-3xl font-light tracking-wide mb-3">
+        {blog.title}
+      </h1>
+      <p className="text-gray-500 mb-6">{blog.date}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: documentToHtmlString(blog.content as any),
+        }}
+      />
+    </article>
   );
 }
